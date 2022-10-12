41ee1c73ab8527eaece5b769554167284115916f
LeandroLicata <leandrolicata1@gmail.com>
2022-10-12 01:17:15 -0300

    Redux

--- 11-Redux/homework/src/reducers/index.js
+++ 11-Redux/homework/src/reducers/index.js
@@ -4,16 +4,36 @@ const initialState = {
   count: 0
 }
 
+const cases = {
+  [INCREMENT]: (state) => {
+    return { count: state.count + 1 };
+
+  },
+  [DECREMENT]: (state) => {
+    return { count: state.count - 1 };
+  },
+};
+
 // Nuestro reducer que maneja nuestros dos casos de acción incremento y decremento.
 // Recibe el estado de nuestro store, junto con una action creada por nuestro action creator. 
 // ¿Qué tiene que hacer el reducer con el contador de cada caso?
 export default (state = initialState, action) => {
-  switch (action.type) {
-    case INCREMENT:
-    // completa para este caso
-    case DECREMENT:
-    // Fill para este otro
-    default:
-      return state;
-  }
+  return cases.hasOwnProperty(action.type) ? cases[action.type](state) : state;
 };
+
+// export default (state = initialState, action) => {
+//   switch (action.type) {
+//     case INCREMENT:
+//     // completa para este caso
+//     return {
+//       count: state.count +1
+//     };
+//     case DECREMENT:
+//     // Fill para este otro
+//     return {
+//       count: state.count -1
+//     };
+//     default:
+//       return state;
+//   }
+// };
